35df03a615b7fd619073d144d01b4bd07abd831d
Chaitan-N <chaitanyanarra80@gmail.com>
2024-07-14 20:02:10 -0400

    feat: bug fixed

--- client/src/pages/game.jsx
+++ client/src/pages/game.jsx
@@ -2,25 +2,28 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import { useParams } from "react-router-dom";
 
-const socket = io();
+const socket = io('localhost:3000');
 
 const Game = () => {
     const { gameID } = useParams();
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
-        console.log('Game page loaded');
-        socket.emit('joinGame', gameID);
-
-        
-        socket.on('counterUpdated', (newCounter) => {
-            console.log('Counter updated:', newCounter); 
-            setCounter(newCounter);
-        });
-
-        return () => {
-            socket.off('counterUpdated');
-        };
+        socket.on('connect', () => {
+            console.log('Game page loaded and socket is connected');
+            socket.emit('joinGame', gameID);
+    
+            
+            socket.on('counterUpdated', (newCounter) => {
+                console.log('Counter updated:', newCounter); 
+                setCounter(newCounter);
+            });
+    
+            return () => {
+                socket.off('counterUpdated');
+            };
+        })
+       
     }, [gameID]);
 
     const handleIncrement = () => {
