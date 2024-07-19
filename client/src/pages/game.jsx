683814e5f1221f1f8b703b054416d469a1f06cc6
ASLaskin <andrewlaskin6@gmail.com>
2024-07-18 20:23:07 -0400

    new style for game

--- client/src/pages/game.jsx
+++ client/src/pages/game.jsx
@@ -1,25 +1,19 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import { useLocation, useParams } from "react-router-dom";
+import './game.css';
 
-const Game = ({}) => {
+const Game = () => {
     const { gameID } = useParams();
     const [socket, setSocket] = useState(null);
     const { state } = useLocation();
     const { name } = state;
     const [opponent, setOpponent] = useState('');
-    const [currentHand, setCurrentHand] = useState('');
-
+    const [currentHand, setCurrentHand] = useState([]);
 
     useEffect(() => {
         const newSocket = io('http://localhost:3000');
 
-        // if (name) {
-        //     console.log('joining game with name:', name);
-        //     newSocket.emit('joinGame', gameID, name);
-        // }else {
-        //    console.log('someone joined using link, time to prompt them for name')
-        // }
         newSocket.emit('joinGame', gameID, name);
         newSocket.on('gameStart', (players) => {
             console.log('game started:', players);
@@ -40,7 +34,6 @@ const Game = ({}) => {
             }
         });
 
-
         setSocket(newSocket);
 
         return () => {
@@ -52,11 +45,27 @@ const Game = ({}) => {
 
 
     return (
-        <div>
-            <h1>Game: {gameID}</h1>
-            <h3>Player: {name}</h3>
-            <p>Current Hand: {currentHand}</p>
-            <h3>Opponent: {opponent.name}</h3>
+        <div className="game-board">
+            <div className="opponent">
+                <h3>Opponent: {opponent.name}</h3>
+            </div>
+            <div className="table">
+                <h1>Game: {gameID}</h1>
+            </div>
+            <div className="player">
+                <h3>Player: {name}</h3>
+                <div className="hand">
+                    {currentHand.map((card, index) => (
+                        <span key={index} className="card">{card}</span>
+                    ))}
+                </div>
+            </div>
+            <div className="controls">
+                <button className="button bg-green-700">Check</button>
+                <button className="button bg-red-700" >Bet</button>
+                <button className="button bg-gray-700" >Call</button>
+                <button className="button bg-purple-700" >Fold</button>
+            </div>
         </div>
     );
 };
